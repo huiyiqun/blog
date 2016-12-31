acc2189ac090cfb5958ca0562b2c40987de6342e
Hui Yiqun <huiyiqun@gmail.com>
2016-12-31 18:43:20 +0800

    change links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('TUNA', 'https://tuna.moe/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/huiyiqun'),
+          ('Telegram', 'https://telegram.me/huiyiqun'),)
 
 DEFAULT_PAGINATION = False
 
