53c64da1d5133470545150c9ceb10488fe5315a6
Hui Yiqun <huiyiqun@gmail.com>
2017-01-01 01:31:08 +0800

    add SITE_URL to enable disqus

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# SITEURL = 'https://blog.huiyiqun.me'
+SITEURL = 'https://blog.huiyiqun.me'
 # RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feed.xml'
