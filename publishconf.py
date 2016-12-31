39641d11f65d96c0bd3ba2e3b1a8d9e7ce6a4c83
Hui Yiqun <huiyiqun@gmail.com>
2016-12-31 19:33:32 +0800

    atom feed at same location as jekyll

--- publishconf.py
+++ publishconf.py
@@ -13,10 +13,7 @@ from pelicanconf import *
 SITEURL = 'https://blog.huiyiqun.me'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+FEED_ALL_ATOM = 'feed.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
